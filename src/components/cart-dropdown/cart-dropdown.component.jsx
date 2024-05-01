c7afa42c4b9eea6f7acd25eb79c8eb50b4e7c81e
EngelBRod <14808288+EngelBRod@users.noreply.github.com>
2024-05-01 13:12:01 -0400

    added Shop Cart Feature

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,20 @@
 import './cart-dropdown.styles.scss'
 import Button from '../button/button.component'
+import CartItem from '../cart-item/cart-item.component'
+import { UserContext } from '../../context/user.context'
+import { CartContext } from '../../context/cart.context'
+import { useContext } from 'react'
 
 const CartDropdown = () => {
+    const { cartItems } = useContext(CartContext)
     return(
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-                <Button>GO TO CHECKOUT</Button>
+                {cartItems.map(item=>{
+                    return(     <CartItem key={item.id} cartItem={item}  /> )
+                })}
             </div>
-
+            <Button>GO TO CHECKOUT</Button>
         </div>
     )
 }
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,19 +1,32 @@
-import './product-card.styles.scss'
+import { useContext } from 'react';
 
-import Button from '../button/button.component'
+import { CartContext } from '../../context/cart.context';
 
-const ProductCard = ({product}) =>{
- const {name, price, imageUrl} = product;
- return(
+import Button from '../button/button.component';
+
+import './product-card.styles.scss';
+
+const ProductCard = ({ product }) => {
+  const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => { 
+    console.log(product);
+    addItemToCart(product);
+  }
+
+  return (
     <div className='product-card-container'>
-    <img src={imageUrl} alt={`${name}`} />
-    <div className='footer'>
+      <img src={imageUrl} alt={`${name}`} />
+      <div className='footer'>
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
+      </div>
+      <Button buttonType='inverted' type='button' onClick={addProductToCart}>
+        Add to card
+      </Button>
     </div>
-    <Button buttonType='inverted'>Add to card</Button>
- </div>
- )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
